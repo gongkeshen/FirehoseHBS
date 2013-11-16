7482543b6c5b0e2498af70999d4f58c1fec46ed0
Gong Ke Gouldstone <fifo@fifo-VirtualBox.(none)>
2013-11-16 01:37:31 -0500

    initial commit: biult a rails application; added splage page; yes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirehoseHbs::Application.config.secret_key_base = '6a107fa97f97d127e2551493bcbb21d6c23c83603889bace638a31aaf198f595bdf6f61f13ce630fa7bdcaeca34b5ab00eb4982d4c9922c8165f66baa54dd4f3'
